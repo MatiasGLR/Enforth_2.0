b4951c4b446f1e61ff3be01fdbc9fbaa09643824
MatiasGLR <matiasgleivas@gmail.com>
2024-10-31 13:34:45 -0300

    a

--- js/personaje.js
+++ js/personaje.js
@@ -1,35 +1,77 @@
-async function read_index_json() {
-    var str = "", str2 = "";
+const raza1_box = document.querySelector("#lista_raza1");
+const raza2_box = document.querySelector("#lista_raza2");
 
-    str = `<div class="">`
+async function cargar_razas() {
+    var str2 = `<div class="">`;
 
-    await $.get("./db/character.json", function(textString) {
+    let nombreraza1="",
+        nombreraza2="",
+        datosraza1="",
+        datosraza2="",
+        imagenraza1="",
+        imagenraza2="",
+        estadisticasraza1="",
+        estadisticasraza2="",
+        habilidad1_1="",
+        habilidad2_1="",
+        habilidad3_1="",
+        habilidad4_1="",
+        habilidad5_1="",
+        habilidad1_2="",
+        habilidad2_2="",
+        habilidad3_2="",
+        habilidad4_2="",
+        habilidad5_2="",
+        raza1det=0,
+        raza2det=0,
+        raza1 = raza1_box.value,
+        raza2 = raza2_box.value,
+        str_imagenes = "";
+    
+
+
+    await $.get("./db/razas.json", function(textString) {
         const index_json = Object.entries(textString);
+        str_imagenes = "";
+        raza1det = 0;
+        raza2det = 0;
         for(let i = 0; i < index_json.length ; i++) {
             const json_a = JSON.parse(JSON.stringify(index_json[i][1]));
-            str2 = str2.concat(`
-            <div class="card bg-dark text-white" style="border-radius:0">
-                <div id="cabecera_`+i+`" class="cabecera_main">
-                    <a id="title_button_info" class="" data-toggle="collapse" data-target="#`+json_a.titulo+`" aria-expanded="true" aria-controls="`+json_a.titulo+`">
-                        <h5 id="title_info" class="mb-0" style="padding: 10px 0 10px 20%">
-                            `+json_a.titulo+`
-                        </h5>
-                    </a>
-                </div>
-                <div id="`+json_a.titulo+`" class="collapse" aria-labelledby="cabecera_`+i+`" data-parent="#listado_datos">
-                    <div class="card-body text-white">
-                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                    </div>
-                </div>
-            </div>
-            `);
+            if(raza1det == 0 && json_a.Nombre == raza1) {
+                console.log(json_a.Nombre+"/"+raza1);
+                nombreraza1 = json_a.Nombre;
+                datosraza1 = json_a.Datos;
+                imagenraza1 = json_a.Imagen;
+                estadisticasraza1 = json_a.Estadisticas;
+                habilidad1_1 = json_a.Habilidad1;
+                habilidad2_1 = json_a.Habilidad2;
+                habilidad3_1 = json_a.Habilidad3;
+                habilidad4_1 = json_a.Habilidad4;
+                habilidad5_1 = json_a.Habilidad5;
+                str_imagenes = str_imagenes.concat(`<img src="./files/personaje/`+imagenraza1+`" style="width:25%" alt="Imagen">`);
+                raza1det = 1;
+            }
+            else if(raza2det == 0 && json_a.Nombre == raza2) {
+                console.log(json_a.Nombre+"/"+raza2);
+                nombreraza2 = json_a.Nombre;
+                datosraza2 = json_a.Datos;
+                imagenraza2 = json_a.Imagen;
+                estadisticasraza2 = json_a.Estadisticas;
+                habilidad1_2 = json_a.Habilidad1;
+                habilidad2_2 = json_a.Habilidad2;
+                habilidad3_2 = json_a.Habilidad3;
+                habilidad4_2 = json_a.Habilidad4;
+                habilidad5_2 = json_a.Habilidad5;
+                str_imagenes = str_imagenes.concat(`<img src="./files/personaje/`+imagenraza2+`" style="width:25%" alt="Imagen">`);
+                raza2det = 1;
+            }
+            if(raza1det == 0) nombreraza1 = "";
+            if(raza2det == 0) nombreraza2 = "";
         }
     });
 
-    str = str.concat(str2,`</div>`);
+    document.querySelector("#raza-imagenes").innerHTML = str_imagenes;
 
-    document.querySelector("#listado_datos").innerHTML = str;
-    console.log(str);
+
+    str2 = str2.concat(str2,`</div>`);
 }
-
-read_index_json();
