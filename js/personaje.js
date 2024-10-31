3a673e710ccd9a8af83e6cbfc2419c25ef5a8781
MatiasGLR <matiasgleivas@gmail.com>
2024-10-31 15:54:26 -0300

    a

--- js/personaje.js
+++ js/personaje.js
@@ -1,6 +1,7 @@
 const raza1_box = document.querySelector("#lista_raza1");
 const raza2_box = document.querySelector("#lista_raza2");
 
+
 async function cargar_razas() {
     var str2 = `<div class="">`;
 
@@ -12,11 +13,13 @@ async function cargar_razas() {
         imagenraza2="",
         estadisticasraza1="",
         estadisticasraza2="",
+        habilidad0_1="",
         habilidad1_1="",
         habilidad2_1="",
         habilidad3_1="",
         habilidad4_1="",
         habilidad5_1="",
+        habilidad0_2="",
         habilidad1_2="",
         habilidad2_2="",
         habilidad3_2="",
@@ -26,8 +29,10 @@ async function cargar_razas() {
         raza2det=0,
         raza1 = raza1_box.value,
         raza2 = raza2_box.value,
-        str_imagenes = "";
-    
+        str_imagenes = "",
+        str_datos = "",
+        str_stats = "",
+        str_skills = "";
 
 
     await $.get("./db/razas.json", function(textString) {
@@ -43,12 +48,18 @@ async function cargar_razas() {
                 datosraza1 = json_a.Datos;
                 imagenraza1 = json_a.Imagen;
                 estadisticasraza1 = json_a.Estadisticas;
+                habilidad0_1 = json_a.HabilidadP;
                 habilidad1_1 = json_a.Habilidad1;
                 habilidad2_1 = json_a.Habilidad2;
                 habilidad3_1 = json_a.Habilidad3;
                 habilidad4_1 = json_a.Habilidad4;
                 habilidad5_1 = json_a.Habilidad5;
-                str_imagenes = str_imagenes.concat(`<img src="./files/personaje/`+imagenraza1+`" style="width:25%" alt="Imagen">`);
+                if(str_datos == "") str_datos = "<b class='text-warning'>Datos de raza</b><br>";
+                if(str_stats == "") str_stats = "<b class='text-warning'>Estadisticas</b><br>";
+                if(str_skills == "") str_skills = "<b class='text-warning'>Habilidades</b><br>";
+                str_imagenes = str_imagenes.concat(`<img src="./files/personaje/`+imagenraza1+`" style="width:250px; margin:4px" alt="Imagen">`);
+                str_datos = str_datos.concat(datosraza1+`<br><br>`);
+                str_stats = str_stats.concat(estadisticasraza1+`<br><br>`);
                 raza1det = 1;
             }
             else if(raza2det == 0 && json_a.Nombre == raza2) {
@@ -57,21 +68,50 @@ async function cargar_razas() {
                 datosraza2 = json_a.Datos;
                 imagenraza2 = json_a.Imagen;
                 estadisticasraza2 = json_a.Estadisticas;
+                habilidad0_2 = json_a.HabilidadP;
                 habilidad1_2 = json_a.Habilidad1;
                 habilidad2_2 = json_a.Habilidad2;
                 habilidad3_2 = json_a.Habilidad3;
                 habilidad4_2 = json_a.Habilidad4;
                 habilidad5_2 = json_a.Habilidad5;
-                str_imagenes = str_imagenes.concat(`<img src="./files/personaje/`+imagenraza2+`" style="width:25%" alt="Imagen">`);
+                if(str_datos == "") str_datos = "<b class='text-warning'>Datos de raza</b><br>";
+                if(str_stats == "") str_stats = "<b class='text-warning'>Estadisticas</b><br>";
+                if(str_skills == "") str_skills = "<b class='text-warning'>Habilidades</b><br>";
+                str_imagenes = str_imagenes.concat(`<img src="./files/personaje/`+imagenraza2+`" style="width:250px; margin:4px" alt="Imagen">`);
+                str_datos = str_datos.concat(datosraza2+`<br><br>`);
+                str_stats = str_stats.concat(estadisticasraza2+`<br><br>`);
                 raza2det = 1;
             }
-            if(raza1det == 0) nombreraza1 = "";
-            if(raza2det == 0) nombreraza2 = "";
+            if(raza1det == 0) nombreraza1="";
+            if(raza2det == 0) nombreraza2="";
         }
     });
 
-    document.querySelector("#raza-imagenes").innerHTML = str_imagenes;
+    if(raza1det != 0 || raza2det != 0) {
+        str_skills = str_skills.concat(`<b class="text-info">Habilidad Pasiva</b><br>`);
+        if(raza1det) str_skills = str_skills.concat(habilidad0_1+`<br>`);
+        if(raza2det) str_skills = str_skills.concat(habilidad0_2+`<br>`);
+        str_skills = str_skills.concat(`<b class="text-info">Nivel 1</b><br>`);
+        if(raza1det) str_skills = str_skills.concat(habilidad1_1+`<br>`);
+        if(raza2det) str_skills = str_skills.concat(habilidad1_2+`<br>`);
+        str_skills = str_skills.concat(`<b class="text-info">Nivel 2</b><br>`);
+        if(raza1det) str_skills = str_skills.concat(habilidad2_1+`<br>`);
+        if(raza2det) str_skills = str_skills.concat(habilidad2_2+`<br>`);
+        str_skills = str_skills.concat(`<b class="text-info">Nivel 3</b><br>`);
+        if(raza1det) str_skills = str_skills.concat(habilidad3_1+`<br>`);
+        if(raza2det) str_skills = str_skills.concat(habilidad3_2+`<br>`);
+        str_skills = str_skills.concat(`<b class="text-info">Nivel 4</b><br>`);
+        if(raza1det) str_skills = str_skills.concat(habilidad4_1+`<br>`);
+        if(raza2det) str_skills = str_skills.concat(habilidad4_2+`<br>`);
+        str_skills = str_skills.concat(`<b class="text-info">Nivel 5</b><br>`);
+        if(raza1det) str_skills = str_skills.concat(habilidad5_1+`<br>`);
+        if(raza2det) str_skills = str_skills.concat(habilidad5_2+`<br>`);
+    }
 
+    document.querySelector("#raza-imagenes").innerHTML = str_imagenes;
+    document.querySelector("#raza-datos").innerHTML = str_datos;
+    document.querySelector("#raza-estadisticas").innerHTML = str_stats;
+    document.querySelector("#raza-habilidades").innerHTML = str_skills;
 
     str2 = str2.concat(str2,`</div>`);
 }
