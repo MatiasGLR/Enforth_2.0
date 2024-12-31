b713d35d3380f583843b4ea1a2c5b6a2680c2326
MatiasGLR <matiasgleivas@gmail.com>
2024-12-31 18:44:57 -0300

    a

--- js/objetos.js
+++ js/objetos.js
@@ -5,7 +5,7 @@ let carrito = [];
 let carrito_compra = [];
 
 async function read_index_json() {
-    var str = "", str2 = "";
+    let str2 = "", lista_objetos = "";
 
     await $.get("./db/items.json", function(textString) {
         const index_json = Object.entries(textString);
@@ -14,6 +14,10 @@ async function read_index_json() {
             nombre_val = input_nombre.value;
             let yes = 1;
             const json_a = JSON.parse(JSON.stringify(index_json[i][1]));
+            let li_pv = 0, li_pc = 0;
+            if(json_a.preciocompra) li_pc = Number(json_a.preciocompra);
+            if(json_a.precioventa) li_pv = Number(json_a.precioventa);
+            lista_objetos = lista_objetos.concat(`<option data-nombre='`+json_a.nombre+`' data-compra='`+li_pc+`' data-venta='`+li_pv+`'>`+json_a.nombre+`</option>`);
             if(categoria_val != "") { if(!json_a.categoria.toLowerCase().includes(categoria_val.toLowerCase())) yes = 0; }
             if(nombre_val != "") { if(!json_a.nombre.toLowerCase().includes(nombre_val.toLowerCase()) && !json_a.descripcion.toLowerCase().includes(nombre_val.toLowerCase()) ) yes = 0; }
             if(yes == 1) {
@@ -27,7 +31,7 @@ async function read_index_json() {
             }
         }
     });
-
+    document.querySelector("#agregado_rapido").innerHTML = lista_objetos;
     document.querySelector("#listado_datos").innerHTML = str2;
 }
 
@@ -71,7 +75,7 @@ async function cargar_objeto(objeto) {
                 document.querySelector("#datos_objeto").innerHTML = str;
                 document.querySelector("#botones_agregar_carrito").innerHTML = `
                         <input id="cantidad_agregar" type="number" min='0' max='1000' style="width:80px" placeholder="Cantidad">
-                        <button onclick="agregaritem_compra('`+json_a.nombre+`', `+json_a.precioventa+`)" type="button" class="btn btn-info" data-bs-dismiss="modal">A la compra</button>
+                        <button onclick="agregaritem_compra('`+json_a.nombre+`', `+json_a.preciocompra+`)" type="button" class="btn btn-info" data-bs-dismiss="modal">A la compra</button>
                         <button onclick="agregaritem('`+json_a.nombre+`', `+json_a.precioventa+`)" type="button" class="btn btn-success" data-bs-dismiss="modal">A la venta</button>
                         <button onclick="modal()" type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>`;
                 modal();
@@ -121,6 +125,10 @@ function lista_tienda() {
             </div>`);
         monto_total = monto_total+(item.precio*item.cantidad);
     });
+    let descuento = document.querySelector("#descuento").value;
+    if(descuento == 0) {
+        $("#descuento").val("");
+    } else monto_total = Math.round(monto_total*(1-(descuento)/100));
     document.querySelector("#monto-total").innerHTML = '<b>Total:</b> $'+monto_total;
     if(monto_total == 0) str = 'Agrega objetos a la lista para vender';
     document.querySelector("#datos_tienda").innerHTML = str;
@@ -142,6 +150,10 @@ function lista_tienda_compra() {
             </div>`);
         monto_total_compra = monto_total_compra+(item.precio*item.cantidad);
     });
+    let descuento = document.querySelector("#descuento_compra").value;
+    if(descuento == 0) {
+        $("#descuento_compra").val("");
+    } else monto_total = Math.round(monto_total*(1-(descuento)/100));
     document.querySelector("#monto-total_compra").innerHTML = '<b>Total:</b> $'+monto_total_compra;
     if(monto_total_compra == 0) str = 'Agrega objetos a la lista para comprar';
     document.querySelector("#datos_tienda_compra").innerHTML = str;
@@ -170,6 +182,46 @@ function objeto_tienda(nombre, cantidad, precio) {
     this.precio = precio;
 }
 
+async function agregaritemrapido_compra(){
+    var nombre = $('#inputrapido_compra').val()
+    var precio = $('#agregado_rapido option').filter(function() {return this.value == nombre;}).data('compra');
+
+    let yes = 1;
+    const cantidad = Number(1);
+    if(Number(cantidad) && cantidad >= 1){
+        carrito_compra.forEach(function (item){
+            if(item.nombre == nombre) {
+                yes = 0;
+            }
+        });
+        if(yes == 1) carrito_compra.push(new objeto_tienda_compra(nombre, cantidad, precio));
+        lista_tienda_compra();
+        document.querySelector("#inputrapido_compra").value = "";
+    } else {
+        $("#cantidad_agregar").css("background-color", "#cc0000");
+    }
+}
+
+async function agregaritemrapido(){
+    var nombre = $('#inputrapido').val()
+    var precio = $('#agregado_rapido option').filter(function() {return this.value == nombre;}).data('venta');
+
+    let yes = 1;
+    const cantidad = Number(1);
+    if(Number(cantidad) && cantidad >= 1){
+        carrito.forEach(function (item){
+            if(item.nombre == nombre) {
+                yes = 0;
+            }
+        });
+        if(yes == 1) carrito.push(new objeto_tienda(nombre, cantidad, precio));
+        lista_tienda();
+        document.querySelector("#inputrapido").value = "";
+    } else {
+        $("#cantidad_agregar").css("background-color", "#cc0000");
+    }
+}
+
 function agregaritem(nombre, precio){
     let yes = 1;
     const cantidad = document.querySelector("#cantidad_agregar").value;
@@ -190,8 +242,8 @@ function agregaritem(nombre, precio){
 function cantidad(nombre, cantidad){
     carrito.forEach(function (item){
         if(item.nombre == nombre) {
-            item.cantidad = item.cantidad+cantidad;
-            if(item.cantidad <= 0) {
+            item.cantidad = Number(item.cantidad)+cantidad;
+            if(Number(item.cantidad) <= 0) {
                 const index = carrito.indexOf(item);
                 carrito.splice(index, 1);
             }
@@ -223,6 +275,26 @@ function objeto_tienda_compra(nombre, cantidad, precio) {
     this.precio = precio;
 }
 
+async function agregaritemrapido_compra(){
+    var nombre = $('#inputrapido_compra').val()
+    var precio = $('#agregado_rapido option').filter(function() {return this.value == nombre;}).data('compra');
+
+    let yes = 1;
+    const cantidad = Number(1);
+    if(Number(cantidad) && cantidad >= 1){
+        carrito_compra.forEach(function (item){
+            if(item.nombre == nombre) {
+                yes = 0;
+            }
+        });
+        if(yes == 1) carrito_compra.push(new objeto_tienda_compra(nombre, cantidad, precio));
+        lista_tienda_compra();
+        document.querySelector("#inputrapido_compra").value = "";
+    } else {
+        $("#cantidad_agregar").css("background-color", "#cc0000");
+    }
+}
+
 function agregaritem_compra(nombre, precio){
     let yes = 1;
     const cantidad = document.querySelector("#cantidad_agregar").value;
@@ -243,8 +315,8 @@ function agregaritem_compra(nombre, precio){
 function cantidad_compra(nombre, cantidad){
     carrito_compra.forEach(function (item_compra){
         if(item_compra.nombre == nombre) {
-            item_compra.cantidad = item_compra.cantidad+cantidad;
-            if(item_compra.cantidad <= 0) {
+            item_compra.cantidad = Number(item_compra.cantidad)+cantidad;
+            if(Number(item_compra.cantidad) <= 0) {
                 const index = carrito_compra.indexOf(item_compra);
                 carrito_compra.splice(index, 1);
             }
